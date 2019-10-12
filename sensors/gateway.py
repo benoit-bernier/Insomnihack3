c0d316af16f7bdf949277697066d6580195c7720
Brice L'helgouarc'h <gagamiaou@gmail.com>
2019-10-12 07:28:55 +0200

    correct phail with URL

--- sensors/gateway.py
+++ sensors/gateway.py
@@ -11,7 +11,7 @@ host_to_fwd = "192.168.43.21:5000"
 def forward(path):
 	try:
 		# TODO test path forwarding (/data) !
-		a = requests.post('http://' + host_to_fwd + path, data=request.form).content # TODO POST, see return value (should be 200 OK !)
+		a = requests.post('http://' + host_to_fwd + '/' + path, data=request.form).content # TODO POST, see return value (should be 200 OK !)
 		print("forwarding data to " + host_to_fwd + "...")
 	except Exception as e:
 		print(e)
