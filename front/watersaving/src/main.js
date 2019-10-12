e7a84c5cfb8623eaae3eb2e437aa28602fcab6b7
Julien.gueperoux <julien.gueperoux@waryme.com>
2019-10-12 05:10:47 +0200

    add axios and google map info windows

--- front/watersaving/src/main.js
+++ front/watersaving/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
+import axios from 'axios'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
@@ -8,6 +9,13 @@ Vue.config.productionTip = false
 
 Vue.use(vuetify, {
   iconfont: 'md',
+})
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAP_API,
+    libraries: 'places', 
+  }
 })
 
 new Vue({
