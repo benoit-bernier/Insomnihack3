a4c209981c9b4d131268c02b8273d21cef2008bc
Yztoc <yztoc@yztoc-PC>
2019-10-12 05:24:46 +0200

    add new setting and charts

--- front/watersaving/src/main.js
+++ front/watersaving/src/main.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
+//import * as VueGoogleMaps from 'vue2-google-maps'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.config.productionTip = false
 
 Vue.use(vuetify, {
   iconfont: 'md',
 })
+
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
 
 new Vue({
   vuetify,
--- front/watersaving/src/router.js
+++ front/watersaving/src/router.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Map from '@/components/Map'
+import Stats from '@/views/Stats'
+import Settings from '@/views/Settings'
+
 Vue.use(Router)
 
 
 export default new Router({
-  mode : 'history',
-    routes: [
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Map,
+    },
     {
       path: '/map',
       name: 'map',
       component: Map,
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: Stats,
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: Settings,
     }
   ]
 })
