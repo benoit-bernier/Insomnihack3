edf0bb42f9055ea93234a7f70acfac75d616724a
AirJulien <julien.gueperoux@waryme.com>
2019-12-07 18:16:21 +0100

    auth

--- back/index.js
+++ back/index.js
@@ -16,51 +16,55 @@ app.use(bodyParser.json()); // parse application/json
 app.use(cors());
 app.use(express.static('static'));
 
-router(app,auth);
+router(app, auth);
 
 var db = require('./config/config');
-console.log('dburl : '+db.url)
-mongoose.connect(db.url,{ useNewUrlParser: true }); 
+console.log('dburl : ' + db.url)
+mongoose.connect(db.url, {
+  useNewUrlParser: true
+});
 
 mongoose.connection.on('connected', () => {
-   console.log('MongoDB connected');
+  console.log('MongoDB connected');
 });
 
 mongoose.connection.on('error', (err) => {
-   console.log('MongoDB connection error : ' + err);
+  console.log('MongoDB connection error : ' + err);
 });
 
 mongoose.connection.on('disconnected', () => {
-   console.log('MongoDB connection close')
+  console.log('MongoDB connection close')
 });
 
-const wss = new WebSocket.Server({ port: 3001 })
+const wss = new WebSocket.Server({
+  port: 3001
+})
 
 wss.on('connection', ws => {
-      console.log('Connected');
-
-      let auth_timeout = setTimeout(function () {
-        console.log("timeout lancé")
-        ws.close();
-      }, 5000);
-      ws.on('message', message => {
-        try {
-          message = JSON.parse(message)
-        } catch (e) {
-          ws.close();
-        }
-        if (message.token != null) {
-          clearTimeout(auth_timeout);
-          wsAuth.authenticate(message.token, (err, result, option) => {
-            if (err) ws.close();
-            else {
-              ws.emit("authenticated")
-              socketTab.push(ws);
-              exports.socketTab = socketTab;
-            }
-          })
+  console.log('Connected');
+  // timer of auth, client has to send the auth message within 5 seconds
+  let auth_timeout = setTimeout(function () {
+    console.log("timeout lancé")
+    ws.close();
+  }, 5000);
+  ws.on('message', message => {
+    try {
+      message = JSON.parse(message)
+    } catch (e) {
+      ws.close();
+    }
+    if (message.token != null) {
+      clearTimeout(auth_timeout);
+      wsAuth.authenticate(message.token, (err, result, option) => {
+        if (err) ws.close();
+        else {
+          ws.emit("authenticated")
+          socketTab.push(ws);
+          exports.socketTab = socketTab;
         }
       })
+    }
+  })
   ws.on('error', message => {
     console.log(`Received message => ${message}`)
   })
@@ -77,13 +81,5 @@ server.listen(3000, function () {
   console.log('server listening on port 3000');
 });
 
-exports.app=app;
-exports.socketTab=socketTab;
-
-
-
-
-
-
-
-
+exports.app = app;
+exports.socketTab = socketTab;
